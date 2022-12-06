09ba353b13783b0eaae39dc40fbccd4624ef7d10
lucinda <lucinda.souchet@gmail.com>
2022-12-06 18:39:00 +0100

    add Projects route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ProjectsView from "../views/ProjectsView.vue";
 
 const routes = [
   {
@@ -15,6 +16,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/projects",
+    name: "Projects",
+    component: ProjectsView,
   },
 ];
 
