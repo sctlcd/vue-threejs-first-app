fe66ed28f48c9757925e525181c60438c84e707f
sctlcd <lucinda.souchet@gmail.com>
2023-01-18 16:56:44 +0100

    create 3D scene: add camera, lights, scene

---
+++ src/World/components/objects/lights.js
@@ -0,0 +1,10 @@
+import { DirectionalLight, DirectionalLightHelper } from "three";
+function createLights(color) {
+  const light = new DirectionalLight(color, 4);
+  const lightHelper = new DirectionalLightHelper(light, 0);
+  light.position.set(0, 0, 5);
+  // eslint-disable-next-line no-unused-vars
+  light.tick = (delta) => {};
+  return { light, lightHelper };
+}
+export { createLights };
